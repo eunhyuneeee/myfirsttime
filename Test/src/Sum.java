142d1c0914d760db14f88dbda5be1f29f385e780
eunhyuneeee <eunhyun25@gmail.com>
2015-10-13 00:00:20 +0900

    sum 클래스 추가

---
+++ Test/src/Sum.java
@@ -0,0 +1,10 @@
+
+public class Sum {
+
+	int a=3;
+	int b=2;
+	
+	int sum(){
+		return a+b;
+	}
+}
--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,5 +2,7 @@
 public class Test {
 	public static void main(String[] args){
 		System.out.println("테스트를합니다");
+		Sum test1 = new Sum();
+		System.out.println(test1.sum());
 	}
 }
